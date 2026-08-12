bfed23369ba26afd06069d4fffb19a2d6a36d1e2
Jaizilantonyj <jaizilantonyj@gmail.com>
2026-08-12 18:24:13 +0530

    Time: 1124 ms (5.13%), Space: 288.6 MB (6.1%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -4,22 +4,46 @@ public:
         if (s.length() < 10)
             return {};
 
-        unordered_set<string> seen;
-        unordered_set<string> repeated;
+        unordered_set<int> seen;
+        unordered_set<int> repeated;
         vector<string> ans;
 
-        for (int i = 0; i <= s.length() - 10; i++) {
-            string temp = s.substr(i, 10);
+        auto encode = [](char c) {
+            if (c == 'A') return 0;
+            if (c == 'C') return 1;
+            if (c == 'G') return 2;
+            return 3; // T
+        };
 
-            if (seen.count(temp)) {
-                repeated.insert(temp);
-            } else {
-                seen.insert(temp);
+        for (int i = 0; i <= s.length() - 10; i++) {
+            int code = 0;
+
+            for (int j = i; j < i + 10; j++) {
+                code = (code << 2) | encode(s[j]);
             }
+
+            if (seen.count(code))
+                repeated.insert(code);
+            else
+                seen.insert(code);
         }
 
-        for (const string& str : repeated) {
-            ans.push_back(str);
+        for (int code : repeated) {
+            string dna;
+
+            for (int i = 0; i < 10; i++) {
+                int x = code & 3;
+
+                if (x == 0) dna += 'A';
+                else if (x == 1) dna += 'C';
+                else if (x == 2) dna += 'G';
+                else dna += 'T';
+
+                code >>= 2;
+            }
+
+            reverse(dna.begin(), dna.end());
+            ans.push_back(dna);
         }
 
         return ans;
